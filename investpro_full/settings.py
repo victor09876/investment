0b1f490cc849ba3a02968a49cb7d2f9f16278d2c
victor09876 <adimekwevictorchinemere@gmail.com>
2026-06-22 19:11:55 +0100

    update feature

--- investpro_full/settings.py
+++ investpro_full/settings.py
@@ -7,8 +7,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-investpro-secret-key-change-in-production'
 DEBUG = True
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['investpro-m02d.onrender.com', 'localhost','127.0.0.1', 'www.investprotrade.com', 'www.investprotrade.com']
-
+ALLOWED_HOSTS = [
+    "investprotrade.com",
+    "www.investprotrade.com",
+    ".onrender.com",
+    'localhost',
+    '127.0.0.1',
+]
 
 # DATABASES = {
 #     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
