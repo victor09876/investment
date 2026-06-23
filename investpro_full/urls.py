cf1a94e89a650cbdca079bc63707482c863bf25a
victor09876 <adimekwevictorchinemere@gmail.com>
2026-06-23 17:03:53 +0100

    update feature

--- investpro_full/urls.py
+++ investpro_full/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = 'accounts.front_views.handler404'
